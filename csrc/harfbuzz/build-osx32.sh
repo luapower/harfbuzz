dd7042b37e8d0c6653cca859d93cc0376a203cb0
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-19 23:29:29 +0200

    osx32 build

--- csrc/harfbuzz/build-osx32.sh
+++ csrc/harfbuzz/build-osx32.sh
@@ -1,7 +1,7 @@
 # harfbuzz build with opentype, ucdn, freetype. dynamically links to ucdn and freetype.
 # TODO: remove the __MINGW32__ hack
-export CC="gcc -arch x86_64"
+export CC="gcc -arch i386"
 export CFLAGS="-O2 -DHAVE_INTEL_ATOMIC_PRIMITIVES -D__MINGW32__"
-export PLATFORM=osx64
+export PLATFORM=osx32
 export LIBNAME=libharfbuzz.dylib
 ./build.sh
