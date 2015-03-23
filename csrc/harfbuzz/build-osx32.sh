09492915a0b6f8a2a2a4b3d7326ad8b7086410e0
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-03-24 00:02:10 +0200

    unimportant

--- csrc/harfbuzz/build-osx32.sh
+++ csrc/harfbuzz/build-osx32.sh
@@ -1,2 +1,2 @@
-P=osx32 C="-arch i386" L="-arch i386 -install_name @loader_path/libharfbuzz.dylib" \
+P=osx32 C="-arch i386" L="-arch i386 -install_name @rpath/libharfbuzz.dylib" \
 	D=libharfbuzz.dylib A=libharfbuzz.a ./build.sh
--- csrc/harfbuzz/build-osx64.sh
+++ csrc/harfbuzz/build-osx64.sh
@@ -1,2 +1,2 @@
-P=osx64 C="-arch x86_64" L="-arch x86_64 -install_name @loader_path/libharfbuzz.dylib" \
+P=osx64 C="-arch x86_64" L="-arch x86_64 -install_name @rpath/libharfbuzz.dylib" \
 	D=libharfbuzz.dylib A=libharfbuzz.a ./build.sh
